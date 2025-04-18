65b0cbaf2a64828ebc4c85a569ca16cce8abbb41
Sufhal <jules.girold@gmail.com>
2025-04-18 16:55:48 +0200

    edit: configuration change & gallery fix

--- src/components/gallery/Gallery.tsx
+++ src/components/gallery/Gallery.tsx
@@ -1,1 +1,15 @@
-export function Gallery() {}
+import Image from "next/image";
+
+type GalleryProps = {
+  images: string[];
+};
+
+export function Gallery(props: React.PropsWithoutRef<GalleryProps>) {
+  return (
+    <div>
+      {props.images.map((v, idx) => (
+        <Image key={idx} src={v} alt="" />
+      ))}
+    </div>
+  );
+}
